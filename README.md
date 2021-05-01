32cb703d4d8ec1592707f0d18093bef054bdc63d
Norapat Buppodom <new17353@gmail.com>
2021-05-01 18:21:58 +0700

    feat: add personalizatin service

---
+++ README.md
@@ -0,0 +1,5 @@
+## Direct connection to PosgresSQL
+
+```
+psql -U username -h localhost -p 5432 dbname
+```
--- main.py
+++ main.py
@@ -6,10 +6,70 @@ import grpc
 import hts.common.common_pb2 as common
 import hts.personalization.service_pb2 as personalization_service
 import hts.personalization.service_pb2_grpc as personalization_service_grpc
+from db_model import (
+    DBSession,
+    UserEvent,
+    EventRecommendation,
+    Event
+)
+from helper import (
+    getInt32Value,
+    getStringValue,
+    getTimeStamp,
+)
+from toolz.dicttoolz import merge_with
+from scipy.special import softmax
+from random import choices
+import numpy as np 
+
 
 class PersonalizationService(personalization_service_grpc.PersonalizationServiceServicer):
     def GetRecommendedEvents(self, request, context):
-        return personalization_service.GetRecommendedEventsResponse(event_collection=[])
+        session = DBSession()
+        try:
+            user_id = request.user_id
+            user_recommendation_score = session.query(UserEvent).filter(UserEvent.user_id == user_id).join(EventRecommendation, UserEvent.event_id == EventRecommendation.event_id).with_entities(EventRecommendation.score)
+
+            dictionary = {}
+            for item in user_recommendation_score:
+                dictionary = merge_with(sum, dictionary, item[0])
+
+            
+            prob = softmax(list(dictionary.values()))
+
+            # events to recommended
+            weight = list(dictionary.values())
+            index = list(dictionary.keys())
+            size = min(4, len(index))
+            choice = np.random.choice(index, p=prob, size=size, replace=False)
+
+            query_events = session.query(Event).filter(Event.id.in_(choice)).all()
+
+            data = map(
+                lambda event: common.Event(
+                    id=event.id,
+                    organization_id=event.organization_id,
+                    location_id=getInt32Value(event.location_id),
+                    description=event.description,
+                    name=event.name,
+                    cover_image_url=getStringValue(event.cover_image_url),
+                    cover_image_hash=getStringValue(event.cover_image_hash),
+                    poster_image_url=getStringValue(event.poster_image_url),
+                    poster_image_hash=getStringValue(event.poster_image_hash),
+                    profile_image_url=getStringValue(event.profile_image_url),
+                    profile_image_hash=getStringValue(event.profile_image_hash),
+                    attendee_limit=event.attendee_limit,
+                    contact=getStringValue(event.contact),
+                    registration_due_date=getTimeStamp(event.registration_due_date),
+                ),
+                query_events,
+            )
+            return personalization_service.GetRecommendedEventsResponse(event_collection=data)
+        except:
+            session.rollback()
+            raise
+        finally:
+            session.close()
 
 port = os.environ.get("GRPC_PORT")
 
@@ -20,6 +80,7 @@ def serve():
     )
     server.add_insecure_port("[::]:" + port)
     server.start()
+    print('service started')
     server.wait_for_termination()
 
 serve()
